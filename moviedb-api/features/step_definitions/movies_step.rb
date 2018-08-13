6a774f9d24f1a355c5c29a46809746f98768046d
Ignacio M <mail@ignacio.tech>
2018-08-13 16:09:31 -0600

    Cucumber tests now working, added rspec

--- moviedb-api/features/step_definitions/movies_step.rb
+++ moviedb-api/features/step_definitions/movies_step.rb
@@ -2,18 +2,19 @@ require_relative '../support/sessionable'
 include Sessionable
 
 When(/^I authenticate for movies data$/) do
-  user = User.find_by(email: 'user1@moviedb.com')
-        puts '*************'
-        puts user.id
-        puts Knock::AuthToken.new(payload: { sub: 1 }).inspect
-        token = Knock::AuthToken.new(payload: { sub: user.id }).token
-  #header 'Authorization', auth_key_for_user('user1@moviedb.com')
-  header 'Authorization', "Bearer #{token}"
+  #user = User.find_by(email: 'user1@moviedb.com')
+   #3     puts '*************'
+     #   puts user.id
+     #   byebug
+     #   puts Knock::AuthToken.new(payload: { sub: 1 }).inspect
+     #   token = Knock::AuthToken.new(payload: { sub: user.id }).token
+  header 'Authorization', auth_key_for_user('user1@moviedb.com')
+  #header 'Authorization', "Bearer #{token}"
 end
 
 Then(/^I should find the response schema consistent for movies index action$/) do
   response = get('/api/v1/movies/')
-
-  row_node_schema = YAML.load(File.read('features/support/schemas/api/v1/movies/index.yml'))
+  movie_node_schema = YAML.load(File.read('features/support/schemas/api/v1/movies/index.yml'))
+  
   JSON::Validator.validate!(row_node_schema, response.body, strict: true) unless Oj.load(response.body).blank?
 end
