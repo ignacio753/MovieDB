7a8df2cb5105ff68e615955ca8d938d5d3f77480
Ignacio M <mail@ignacio.tech>
2018-08-19 16:57:55 +0200

    Add Heroku config

--- moviedb-client/src/components/LogoutForm.js
+++ moviedb-client/src/components/LogoutForm.js
@@ -3,7 +3,7 @@ class LogoutForm extends Component {
   render(){
     return(
       <form className="form-inline my-2 my-lg-0">
-        <li class="nav-item inline log"><p class="navbar-text">Welcome back: {this.props.currentUser.email}</p></li>
+        <li class="nav-item inline log"><p className="navbar-text">Welcome back: {this.props.currentUser.email}</p></li>
         <li class="nav-item inline log"><button className="btn btn-link log my-2 my-sm-0">Log out </button></li>
       </form>
     )
